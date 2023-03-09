e11c32f51a21b9e3e27b2f3bdb1b521bbb3ab685
matteomensialkemy <125556492+matteomensi00@users.noreply.github.com>
2023-03-09 14:13:21 +0100

    Add files via upload
    script curl dataset da github

---
+++ script.sh
@@ -0,0 +1,3 @@
+GIT="https://raw.githubusercontent.com/matteomensi00/finalProject_2/main/trainsampled.csv"
+dataset_zip=$(curl -l https://raw.githubusercontent.com/matteomensi00/finalProject_2/main/trainsampled.csv > trainsampled.csv)
+echo "fatto!"
