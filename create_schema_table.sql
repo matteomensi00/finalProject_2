955bd15441d87a7c6ece2b6fdcc0e26128fcd20e
Tommaso Garofalo <tommaso.garofalo@alkemy.com>
2023-03-09 17:39:14 +0100

    file di test e di creazione schema e tabella sul db

---
+++ create_schema_table.sql
@@ -0,0 +1,25 @@
+
+DROP SCHEMA IF EXISTS
+CREATE SCHEMA IF NOT EXISTS finalproject2
+DROP SCHEMA IF EXISTS
+CREATE TABLE taxi_ny (
+id varchar(20) primary key,
+vendor_id int(3) not null,
+pickup_datetime timestamp not null, 
+dropoff_datetime timestamp not null, 
+passenger_count int not null, 
+pickup_longitude double precision not null,
+pickup_latitude double precision not null,
+dropoff_longitude double precision not null, 
+dropoff_latitude double precision not null,
+store_and_fwd_flag varchar not null,
+trip_duration int not null,
+pickup_street varchar not null,
+pickup_suburb varchar not null,
+pickup_city varchar not null,
+dropoff_street varchar not null,
+dropoff_suburb varchar not null,
+dropoff_city varchar not null,
+distance_miles double precision not null,
+total_cost double precision not null
+)
